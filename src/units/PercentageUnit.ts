6353d551a324a51a85b1b605eea5074e56200a50
Timur <toombez.timurtokaev@gmail.com>
2024-07-19 04:20:26 +0300

    Add PercentageUnit

---
+++ src/units/PercentageUnit.ts
@@ -0,0 +1,27 @@
+import { Branded, clamp } from "../utils"
+import ColorSpaceUnit from "./ColorSpaceUnit"
+
+export default class PercentageUnit extends ColorSpaceUnit<'percentage'> {
+    public getName(): "percentage" {
+        return 'percentage'
+    }
+
+    public setValue(value: number): ColorSpaceUnit<"percentage"> {
+        return new PercentageUnit(value)
+    }
+
+    public parse(value: number): Branded<number, "percentage"> {
+        return clamp(value, {
+            min: this.getMin(),
+            max: this.getMax(),
+        }) as Branded<number, 'percentage'>
+    }
+
+    public getMin(): 0 {
+        return 0
+    }
+
+    public getMax(): 100 {
+        return 100
+    }
+}
