c18b4142129ebadf6e39ef9158f2a926028f9527
Timur <81884993+toombez@users.noreply.github.com>
2024-07-25 16:53:05 +0300

    Feat/docs-for-utilies (#13)
    * Docs types
    
    * Docs inRange utility
    
    * Docs clamp utility
    
    * Docs modulo utility

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,19 +1,61 @@
+/**
+ * Options for setting limits.
+ */
 type LimitOptions = {
-    minimum?: number
-    maximum?: number
+    /**
+     * The minimum value allowed.
+     */
+    minimum: number
+
+    /**
+     * The maximum value allowed.
+     */
+    maximum: number
 }
 
-export const inRange = (value: number, {
-    minimum = Number.NEGATIVE_INFINITY,
-    maximum = Number.POSITIVE_INFINITY,
-}: LimitOptions = {}): boolean => {
+/**
+ * Checks if a given number falls within a specified range including minimum and maximum.
+ *
+ * `options.minimum` fallback is `Number.NEGATIVE_INFINITY`.
+ *
+ * `options.maximum` fallback is `Number.POSITIVE_INFINITY`.
+ *
+ * @param value - Number to be checked against the range.
+ * @param options - Object specifying the range boundaries.
+ * @returns `true` if `value` within range `[minimum, maximum]`, `false` else.
+ */
+export const inRange = (
+    value: number,
+    options: Partial<LimitOptions> = {}
+): boolean => {
+    const {
+        minimum = Number.NEGATIVE_INFINITY,
+        maximum = Number.POSITIVE_INFINITY,
+    } = options
+
     return value >= minimum && value <= maximum
 }
 
-export const clamp = (value: number, {
-    minimum = Number.NEGATIVE_INFINITY,
-    maximum = Number.POSITIVE_INFINITY,
-}: LimitOptions = {}): number =>  {
+/**
+ * Clamps a number within a specified range including minimum and maximum.
+ *
+ * `options.minimum` fallback is `Number.NEGATIVE_INFINITY`.
+ *
+ * `options.maximum` fallback is `Number.POSITIVE_INFINITY`.
+ *
+ * @param value - Number to be clamped.
+ * @param options - Object specifying the range boundaries.
+ * @returns Clamped value, which will be within the range `[minimum, maximum]`.
+ */
+export const clamp = (
+    value: number,
+    options: Partial<LimitOptions> = {}
+): number => {
+    const {
+        minimum = Number.NEGATIVE_INFINITY,
+        maximum = Number.POSITIVE_INFINITY,
+    } = options
+
     if (inRange(value, { minimum, maximum })) {
         return value
     }
@@ -21,10 +63,28 @@ export const clamp = (value: number, {
     return Math.min(Math.max(value, minimum), maximum)
 }
 
-export const modulo = (value: number, {
-    minimum = Number.MIN_SAFE_INTEGER,
-    maximum = Number.MAX_SAFE_INTEGER,
-}: LimitOptions = {}): number => {
+/**
+ * Computes the modulo of a number within a specified range including minimum.
+ *
+ * If any value is Infinity falls with error.
+ *
+ * `options.minimum` fallback is `Number.MIN_SAFE_INTEGER`.
+ *
+ * `options.maximum` fallback is `Number.MAX_SAFE_INTEGER`.
+ *
+ * @param value - Number to compute the modulo for.
+ * @param options - Object specifying the range boundaries.
+ * @returns The value within the range `[minimum, maximum)`.
+ */
+export const modulo = (
+    value: number,
+    options: Partial<LimitOptions> = {}
+): number => {
+    const {
+        minimum = Number.MIN_SAFE_INTEGER,
+        maximum = Number.MAX_SAFE_INTEGER,
+    } = options
+
     if (
         !Number.isFinite(maximum)
         || !Number.isFinite(minimum)
