f9d2ef512bf3e64edc2e2dcb8e88000b54d458f1
Timur <toombez.timurtokaev@gmail.com>
2024-07-30 12:20:47 +0300

    Add function for creating factories

--- src/spaces/color.ts
+++ src/spaces/color.ts
@@ -4,9 +4,11 @@ export const MIN_ALPHA = 0
 export const MAX_ALPHA = 1
 export const DEFAULT_ALPHA = MAX_ALPHA
 
+type ColorComponents = Record<string, number>
+
 export type Color<
     T extends string,
-    C extends Record<string, number>
+    C extends ColorComponents
 > = Readonly<{
     _tag: T
     components: Readonly<C>
@@ -15,7 +17,7 @@ export type Color<
 
 export const createColor = <
     T extends string,
-    C extends Record<string, number>
+    C extends ColorComponents
 >(
     tag: T,
     components: C,
@@ -28,3 +30,35 @@ export const createColor = <
     }) * 100) / 100,
     components: Object.freeze(Object.assign({}, components)),
 })
+
+export type ColorFactory<
+    T extends string,
+    C extends ColorComponents
+> = (data: C & { alpha?: number }) => Color<T, C>
+
+export type ColorComponentsValidators<
+    C extends ColorComponents
+> = {
+    [K in keyof C]: (number: number) => number
+}
+
+export const createColorFactory = <
+    T extends string,
+    C extends ColorComponents
+> (
+    tag: T,
+    componentValidators: ColorComponentsValidators<C>
+): ColorFactory<T, C> => ({
+    alpha,
+    ...rawComponents
+}) =>
+    createColor(
+        tag,
+        Object
+            .entries(componentValidators)
+            .reduce((components, [component, validator]) => ({
+                ...components,
+                [component]: validator(rawComponents[component])
+            }), {} as C),
+        alpha,
+    )
