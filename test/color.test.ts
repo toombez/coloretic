6427cfb476e5a292a64b2297e5e6f060dfd206a7
Timur <toombez.timurtokaev@gmail.com>
2024-07-30 13:46:29 +0300

    Add test for creating colors

--- test/color.test.ts
+++ test/color.test.ts
@@ -2,46 +2,42 @@ import { assert, describe, expect, test } from "vitest"
 import { createColor } from "../src"
 
 describe('generic color', () => {
-    test('creating color result is frozen', () => {
-        const color = createColor('test', {})
-        assert.isFrozen(color)
-    })
+    describe('creating generic color', () => {
+        test('created color is frozen', () => {
+            const color = createColor('test', {})
+            assert.isFrozen(color)
+        })
 
-    test('created color data is frozen', () => {
-        const color = createColor('test', {})
-        assert.isFrozen(color.data)
-    })
+        test('created color component is frozen', () => {
+            const color = createColor('test', {})
+            assert.isFrozen(color.components)
+        })
 
-    test('alpha in range [0, 1]', () => {
-        const transparentColor = createColor('test', {}, -1)
-        const solidColor = createColor('test', {}, 2)
-        const color = createColor('test', {}, 0.5)
+        test('if alpha not passed it will be 1', () => {
+            const color = createColor('test', {})
+            expect(color.alpha).toBe(1)
+        })
 
-        expect(transparentColor.alpha).toBe(0)
-        expect(solidColor.alpha).toBe(1)
-        expect(color.alpha).toBe(0.5)
-    })
+        test('alpha in range of [0, 1]', () => {
+            const color1 = createColor('test', {})
+            expect(color1.alpha).toBeLessThanOrEqual(1)
+            expect(color1.alpha).toBeGreaterThanOrEqual(0)
 
-    test('data passed while creating is new object', () => {
-        const data = {}
-        const color = createColor('test', data)
+            const color2 = createColor('test', {}, 2)
+            expect(color2.alpha).toBeLessThanOrEqual(1)
+            expect(color2.alpha).toBeGreaterThanOrEqual(0)
 
-        expect(color.data).not.toBe(data)
-    })
+            const color3 = createColor('test', {}, -1)
+            expect(color3.alpha).toBeLessThanOrEqual(1)
+            expect(color3.alpha).toBeGreaterThanOrEqual(0)
 
-    test('changing simple raw data object not create color side effects', () => {
-        const simpleData = { foo: 'bar' }
-        const simpleColor = createColor('test', simpleData)
+            const color4 = createColor('test', {}, Infinity)
+            expect(color4.alpha).toBeLessThanOrEqual(1)
+            expect(color4.alpha).toBeGreaterThanOrEqual(0)
 
-        simpleData.foo = 'baz'
-        expect(simpleColor.data.foo).toBe('bar')
-    })
-
-    test.todo('changing complex raw data object not create color side effects', () => {
-        const complexData = { foo: { bar: 'baz' } }
-        const complexColor = createColor('test', complexData, 1)
-
-        complexData.foo.bar = 'new baz'
-        expect(complexColor.data.foo.bar).not.toBe('new baz')
+            const color5 = createColor('test', {}, Infinity)
+            expect(color5.alpha).toBeLessThanOrEqual(1)
+            expect(color5.alpha).toBeGreaterThanOrEqual(0)
+        })
     })
 })
