c7cb4e2208ec693fdc604806f69e63dad6c66ce6
Timur <toombez.timurtokaev@gmail.com>
2024-07-30 12:23:24 +0300

    Create factory for rgb

--- src/spaces/rgb.ts
+++ src/spaces/rgb.ts
@@ -1,40 +1,32 @@
 import { clamp } from "../utils"
-import { Color, createColor } from "./color"
+import { Color, createColorFactory } from "./color"
 
 export const MIN_RGB_COMPONENT = 0
 export const MAX_RGB_COMPONENT = 255
 
 export const RGB_TAG = 'RGB'
 
-export type RGBColor = Color<typeof RGB_TAG, {
+export type RGBComponents = {
     red: number
     green: number
     blue: number
-}>
+}
 
-export const createRGBColor = (
-    red: number,
-    green: number,
-    blue: number,
-    alpha?: number,
-): RGBColor => createColor(
-    RGB_TAG,
-    {
-        red: Math.round(clamp(red, {
-            minimum: MIN_RGB_COMPONENT,
-            maximum: MAX_RGB_COMPONENT,
-        })),
-        green: Math.round(clamp(green, {
-            minimum: MIN_RGB_COMPONENT,
-            maximum: MAX_RGB_COMPONENT,
-        })),
-        blue: Math.round(clamp(blue, {
-            minimum: MIN_RGB_COMPONENT,
-            maximum: MAX_RGB_COMPONENT,
-        })),
-    },
-    alpha,
-)
+export type RGBColor = Color<typeof RGB_TAG, RGBComponents>
+
+const validateRGBComponent = (value: number) => Math.round(clamp(value, {
+    minimum: MIN_RGB_COMPONENT,
+    maximum: MAX_RGB_COMPONENT,
+}))
+
+export const createRGBColor = createColorFactory<
+    typeof RGB_TAG,
+    RGBComponents
+>(RGB_TAG, {
+    red: validateRGBComponent,
+    green: validateRGBComponent,
+    blue: validateRGBComponent,
+})
 
 export const isRGBColor = (
     color: Color<string, any>,
